fe9f8006715ebe04aa21c21e4f20a6d2bd07277a
Joel Lefkowitz <joel.lefkowitz@saltpay.co>
2023-05-23 20:09:46 +0100

    Fix mypy untyped function

--- src/lower.py
+++ src/lower.py
@@ -8,26 +8,25 @@ from .snake import is_snake_case, snake_to_lower_case
 from .title import is_title_case, title_to_lower_case
 from .upper import is_upper_case, upper_to_lower_case
 
-def lower_case(string: str) -> str:
-    try:
-        return next(to_case(string) for is_case, to_case in TO_LOWER if is_case(string))
-
-    # Throwing an error introduces an impurity but is preferable to
-    # returning an empty result.
-    except StopIteration:
-        raise MixedCaseError(string)
-
-
 def is_lower_case(string: str) -> bool:
     return LOWER.match(string) is not None
 
-TO_LOWER = [
-    (is_lower_case, lambda x: x),
-    (is_upper_case, upper_to_lower_case),
-    (is_sentence_case, sentence_to_lower_case),
-    (is_title_case, title_to_lower_case),
-    (is_camel_case, camel_to_lower_case),
-    (is_snake_case, snake_to_lower_case),
-    (is_kebab_case, kebab_to_lower_case),
-    (is_pascal_case, pascal_to_lower_case),
-]
+def lower_case(string: str) -> str:
+    to_lower_case = {
+        is_upper_case: upper_to_lower_case,
+        is_sentence_case: sentence_to_lower_case,
+        is_title_case: title_to_lower_case,
+        is_camel_case: camel_to_lower_case,
+        is_snake_case: snake_to_lower_case,
+        is_kebab_case: kebab_to_lower_case,
+        is_pascal_case: pascal_to_lower_case,
+    }
+
+    if is_lower_case(string):
+        return string
+
+    for is_case, to_case in to_lower_case.items():
+        if is_case(string):
+            return to_case(string)
+
+    raise MixedCaseError(string)
